371d87fbe9d263cf6360d24e8d7308108ba16075
illutek <stefanvandenborne@gmail.com>
2019-03-10 22:46:09 +0100

    bug fix social icons footer

--- templates/pages/partials/footer.inc.php
+++ templates/pages/partials/footer.inc.php
@@ -12,10 +12,10 @@
         <p>&copy; TTHZ CYCLING TEAM <?php echo date(Y); ?> | <a href="<?php print base_path() ?>privacy-verklaring" class="footer__link">Privacy verklaring</a></p>
       </div>
       <div class="footer-wrap__right">
-        <a class="footer__link" href="https://www.facebook.com/groups/135633596623338/">
+        <a class="footer__link--social" href="https://www.facebook.com/groups/135633596623338/">
           <i class="fab fa-facebook-f fa-2x"></i>
         </a>
-        <a class="footer__link" href="https://twitter.com/">
+        <a class="footer__link--social" href="https://twitter.com/">
           <i class="fab fa-twitter fa-2x"></i></i>
         </a>
         <script>mail2("stefan","gmail",0,"","<i class='far fa-envelope fa-2x'></i>")</script>
