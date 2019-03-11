fe9604b6954fdb43c4d3f4844cbae0b8305103de
illutek <stefanvandenborne@gmail.com>
2019-03-11 11:24:06 +0100

    bestuur and leden layout

--- templates/node/node--lid--teaser.tpl.php
+++ templates/node/node--lid--teaser.tpl.php
@@ -1,14 +1,13 @@
 <?php
 /**
  * 2019-01-25
- * 
+ * <h3><?php print $title; ?></h3>
  */
 
 ?>
 
 <div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
-    <div class="leden-node">
-        <h3><?php print $title; ?></h3>
+    <div class="leden-node">        
         <?php print render($content['field_image']); ?>
     </div>
 </div>
--- templates/pages/partials/nav.inc.php
+++ templates/pages/partials/nav.inc.php
@@ -19,11 +19,11 @@
     </div>
     <div id="navbar" class="navbar-collapse collapse">
       <ul class="nav navbar-nav">
-        <li><a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>">Home</a></li>
-        <li><a href="<?php print base_path() ?>tthz-contacteren">Contacteren</a></li>
-        <li><a href="<?php print base_path() ?>#">Bestuur-Team</a></li>
-        <li><a href="<?php print base_path() ?>reglement">Clubregelement</a></li>
-        <li><a href="<?php print base_path() ?>fotoalbums">Fotoalbums</a></li>
+        <li class="navbar__item"><a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>">Home</a></li>
+        <li class="navbar__item"><a href="<?php print base_path() ?>tthz-contacteren">Contacteren</a></li>
+        <li class="navbar__item"><a href="<?php print base_path() ?>bestuur">Bestuur-Team</a></li>
+        <li class="navbar__item"><a href="<?php print base_path() ?>reglement">Clubregelement</a></li>
+        <li class="navbar__item"><a href="<?php print base_path() ?>fotoalbums">Fotoalbums</a></li>
       </ul>
     </div>
   </div>
