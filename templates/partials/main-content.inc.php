086760eb0a67d903a5d7addf54e801935bb92dee
illutek <stefanvandenborne@gmail.com>
2019-04-05 20:59:00 +0200

    clean up code for main-content

--- templates/partials/main-content.inc.php
+++ templates/partials/main-content.inc.php
@@ -13,13 +13,20 @@
         <a id="main-content"></a>
         <?php print render($title_prefix); ?>
         <?php if ($title): ?>
-          <h1 class="title" id="page-title"><?php print $title; ?></h1><?php endif; ?>
+          <h1 class="title" id="page-title"><?php print $title; ?></h1>
+        <?php endif; ?>
         <?php print render($title_suffix); ?>
+
         <?php if ($tabs): ?>
-          <div class="tabs"><?php print render($tabs); ?></div><?php endif; ?>
+          <div class="tabs"><?php print render($tabs); ?></div>
+        <?php endif; ?>
+
         <?php print render($page['help']); ?>
+
         <?php if ($action_links): ?>
-          <ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
+          <ul class="action-links"><?php print render($action_links); ?></ul>
+        <?php endif; ?>
+
         <?php print render($page['content']); ?>
       </div>
       <!-- end content -->
