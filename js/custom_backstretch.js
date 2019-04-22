f3fcb715e72f4dd73d00c2b868982c2df09fe08d
illutek <stefanvandenborne@gmail.com>
2019-04-22 21:42:00 +0200

    header backstretch adjusted on demand

--- js/custom_backstretch.js
+++ js/custom_backstretch.js
@@ -3,8 +3,11 @@
  */
 (function($){
     $(document).ready(function(){
-        var base = window.Drupal.settings.basePath + 'sites/all/themes/tthz/images/';
+        var base = window.Drupal.settings.basePath + 'sites/all/themes/tthz/images/header/';
         $(".bcg_header").backstretch([
+                base + 'tthz-001.jpg',
+                base + 'tthz-002.jpg',
+                base + 'tthz-004.jpg',
                 base + 'cycling-race.jpg',
                 base + 'groot_100702-wielrenners-min.jpg'],
             {fade: 750, duration: 4000});
