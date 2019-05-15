dd167fd9586794c3bc3d3f5e11064f718b9ddcef
illutek <stefanvandenborne@gmail.com>
2019-05-15 11:10:36 +0200

    add submenu links to navigation

--- templates/partials/shared/nav.inc.php
+++ templates/partials/shared/nav.inc.php
@@ -1,6 +1,7 @@
 <?php
  /**
   * SCSS - 03-regions/_nav.scss
+  * <li class="navbar__item"><a href="<?php print base_path() ?>hoe-sponseren">Hoe sponseren</a></li>
   *
   */
 ?>
@@ -19,18 +20,43 @@
     </div>
     <div id="navbar" class="navbar-collapse collapse">
       <ul class="nav navbar-nav">
-        <li class="navbar__item"><a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>">Home</a></li>
-        <li class="navbar__item"><a href="<?php print base_path() ?>tthz-contacteren">Contacteren</a></li>
-        <li class="navbar__item"><a href="<?php print base_path() ?>bestuur">Bestuur-Team</a></li>
-        <li class="navbar__item"><a href="<?php print base_path() ?>reglement">Clubregelement</a></li>
-        <li class="navbar__item"><a href="<?php print base_path() ?>hoe-sponseren">Hoe sponseren</a></li>
-        <li class="navbar__item"><a href="<?php print base_path() ?>fotoalbums">Fotoalbums</a></li>
+        <li class="navbar-nav__item">
+          <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" class="navbar-nav__link">Home</a>
+        </li>
+        <li class="navbar-nav__item">
+          <a href="<?php print base_path() ?>tthz-contacteren" class="navbar-nav__link">Contacteren</a>
+        </li>
+        <li class="navbar-nav__item">
+          <a href="<?php print base_path() ?>bestuur" class="navbar-nav__link">Bestuur-Team</a>
+        </li>
+        <li class="navbar-nav__item">
+          <a href="<?php print base_path() ?>reglement" class="navbar-nav__link">TTHZ-spirit</a>
+        </li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Bolderberg Koerse!
+            <span class="caret"></span>
+          </a>
+            <ul class="dropdown-menu" role="menu">
+              <li class="dropdown-menu__no-link">Toeristen</li>
+              <li><a href="<?php print base_path() ?>evenement/save-date-24082019-bolderberg-koerse">Info</a></li>
+              <li role="separator" class="divider"></li>
+              <li class="dropdown-menu__no-link">Dorpelingen</li>
+              <li><a href="<?php print base_path() ?>evenement/save-date-24082019-bolderberg-dorpelingen-koerse">Info</a></li>
+              
+            </ul>
+        </li>
+        <li class="navbar-nav__item">
+          <a href="<?php print base_path() ?>tthz-kalender" class="navbar-nav__link">Kalender</a>
+        </li>
+        <li class="navbar-nav__item">
+          <a href="<?php print base_path() ?>fotoalbums" class="navbar-nav__link">Fotoalbums</a>
+        </li>
 
         <?php global $user;
           if ($user->uid != 0) { ?>
             <li class="navbar__item"><a href="<?php print base_path() ?>user/logout">Uitloggen</a></li>
           <?php } else { ?>
-            <li class="navbar__item"><a href="<?php print base_path() ?>user">Inloggen</a></li>
+            <li class="navbar__item"><a href="<?php print base_path() ?>user">Leden</a></li>
           <?php } ?>
        
         
