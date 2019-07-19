2e6339e0afaf0416029bf2e41e513d5e573b9826
illutek <stefanvandenborne@gmail.com>
2019-07-19 16:31:48 +0200

    link from panel to read-more btn

--- templates/node/node--view--news-frontpage.tpl.php
+++ templates/node/node--view--news-frontpage.tpl.php
@@ -6,16 +6,14 @@
  *
  */
 ?>
-<a href="<?php print $node_url; ?>" class="news__teaser" <?php print $attributes; ?>>
-
-
+<div href="<?php print $node_url; ?>" class="news__teaser" <?php print $attributes; ?>>
 
   <h2<?php print $title_attributes; ?>><?php print $title; ?></h2>
 
   <div class="content"<?php print $content_attributes; ?>>
     <?php print render($content['body']); ?>
 
-    <div class="news__btn">Lees verder</div>
+    <div class="news__btn"><a href="<?php print $node_url; ?>">Lees verder</a></div>
   </div>
 
-</a>
+</div>
